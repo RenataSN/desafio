7984943f6333d56bf88e8c11d709dc08ebf4dcf5
Renata Silva <renatasilva@macbook-pro-de-renata.local>
2018-12-15 02:58:22 -0400

    Initial Commit

---
+++ Desafio/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Desafio
+//
+//  Created by Renata da Silva Nunes on 15/12/2018.
+//  Copyright © 2018 Renata da Silva Nunes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
